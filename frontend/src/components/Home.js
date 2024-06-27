a65d8c65e8ad8d53cfd0ba20eae55c703b5cdaf5
SriramAllu13 <sriramallu64@gmail.com>
2024-06-28 00:17:07 +0530

    commit

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -85,7 +85,7 @@ const Home = () => {
       <div
         data-theme={theme}
         className={`flex flex-col h-screen ${
-          theme === "wireframe" ? "text-black" : "text-white"
+          theme === "wireframe" ? "text-black bg-white" : "text-white bg-black"
         }`}
       >
         <HomeNav
@@ -93,56 +93,57 @@ const Home = () => {
           theme={theme}
           scrollToTop={scrollToTop}
         />
-
-        <div
-          className={`relative h-screen flex items-center justify-center ${
-            theme === "wireframe" ? "bg-white" : " bg-black"
-          }`}
-        >
+        <div className={`${theme === "wireframe" ? "" : "bg-black"}`}>
           <div
-            className="absolute inset-0 bg-cover bg-center"
-            style={{
-              backgroundImage: theme === "wireframe" ? "" : `url(${left})`,
-              opacity: 0.5,
-            }}
-          ></div>
-          <div className="hero min-h-screen my-10">
-            <div className="hero-content  flex-col lg:flex-row-reverse ">
-              {theme === "wireframe" ? (
-                <img
-                  src={webdev2}
-                  className="w-11/12 md:w-2/3 rounded-lg shadow-2xl"
-                  alt=""
-                />
-              ) : (
-                <img
-                  src={webdev}
-                  className="w-11/12 md:w-2/3 rounded-lg shadow-2xl "
-                  alt=""
-                />
-              )}
-              <div className="text-center">
-                <h1 className="text-3xl md:text-4xl font-bold">WebDev</h1>
-                <p className="py-6 text-base md:text-lg">
-                  An Editor Comprises of HTML CSS JavaScript Integration with
-                  AI.
-                </p>
-                <Link to="/web-editor">
-                  <button className="bg-neutral-950 text-white border border-neutral-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
-                    <span className="bg-neutral-400 shadow-neutral-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
-                    Try Now !
-                  </button>
-                </Link>
+            className={`relative h-screen flex items-center justify-center my-20  ${
+              theme === "wireframe" ? "bg-white" : " bg-black"
+            }`}
+          >
+            <div
+              className="absolute inset-0 bg-cover bg-center"
+              style={{
+                backgroundImage: theme === "wireframe" ? "" : `url(${left})`,
+                opacity: 0.5,
+              }}
+            ></div>
+            <div className="hero min-h-screen">
+              <div className="hero-content  flex-col lg:flex-row-reverse">
+                {theme === "wireframe" ? (
+                  <img
+                    src={webdev2}
+                    className="w-11/12 md:w-2/3 rounded-lg shadow-2xl"
+                    alt=""
+                  />
+                ) : (
+                  <img
+                    src={webdev}
+                    className="w-11/12 md:w-2/3 rounded-lg shadow-2xl "
+                    alt=""
+                  />
+                )}
+                <div className="text-center">
+                  <h1 className="text-3xl md:text-4xl font-bold">WebDev</h1>
+                  <p className="py-6 text-base md:text-lg">
+                    An Editor Comprises of HTML CSS JavaScript Integration with
+                    AI.
+                  </p>
+                  <Link to="/web-editor">
+                    <button className="bg-neutral-950 text-white border border-neutral-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+                      <span className="bg-neutral-400 shadow-neutral-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
+                      Try Now !
+                    </button>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div
-          className={`relative h-screen flex items-center justify-center ${
+          className={`relative h-screen flex items-center justify-center  ${
             theme === "wireframe" ? "bg-white" : " bg-black"
           }`}
         >
-          <div className="hero h-full my-10  ">
+          <div className="hero h-full my-10 ">
             <div className="hero-content text-center">
               <div className="max-w-md">
                 <h1 className=" text-3xl lg:text-4xl text-center flex justify-center gap-3 font-bold ">
@@ -160,6 +161,7 @@ const Home = () => {
             </div>
           </div>
         </div>
+
         <div
           className={`relative h-screen flex items-center justify-center ${
             theme === "wireframe" ? "bg-white" : " bg-black"
@@ -172,7 +174,7 @@ const Home = () => {
               opacity: 0.5,
             }}
           ></div>
-          <div className="hero min-h-screen my-10">
+          <div className="hero min-h-screen">
             <div className="hero-content  flex-col lg:flex-row">
               {theme === "wireframe" ? (
                 <img
@@ -203,169 +205,175 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div
-          className={`relative h-screen flex items-center justify-center  ${
-            theme === "wireframe" ? "bg-white" : "bg-black"
-          }`}
-        >
+        <div className={`${theme === "wireframe" ? "" : "bg-black"}`}>
           <div
-            className="absolute inset-0 bg-cover bg-center"
-            style={{
-              backgroundImage: theme === "wireframe" ? "" : `url(${center})`,
-              opacity: 0.5,
-            }}
-          ></div>
-
-          <div className="carousel w-full relative flex flex-col items-center my-10 gap-10">
-            <div>
-              <h1 className="text-4xl md:text-5xl font-bold ">Snapshots</h1>
-            </div>
-            <Swiper
-              spaceBetween={30}
-              centeredSlides={true}
-              autoplay={{
-                delay: 3000,
-                disableOnInteraction: false,
+            className={`relative h-screen flex items-center justify-center my-20${
+              theme === "wireframe" ? "bg-white" : "bg-black"
+            }`}
+          >
+            <div
+              className="absolute inset-0 bg-cover bg-center"
+              style={{
+                backgroundImage: theme === "wireframe" ? "" : `url(${center})`,
+                opacity: 0.5,
               }}
-              cssMode={true}
-              mousewheel={true}
-              keyboard={true}
-              modules={[Autoplay, Mousewheel, Keyboard]}
-              className="mySwiper w-11/12 md:w-2/3 mx-auto rounded-lg shadow-2xl mb-20"
-              ref={swiperRef}
-            >
-              {slideImagesDark.map((img, index) => (
-                <SwiperSlide key={index}>
-                  {theme === "wireframe" ? (
-                    <img
-                      src={slideImagesWireframe[index]}
-                      alt={`Slide ${index + 1}`}
-                    />
-                  ) : (
-                    <img src={img} alt={`Slide ${index + 1}`} />
-                  )}
-                </SwiperSlide>
-              ))}
-            </Swiper>
+            ></div>
 
-            <div className="absolute hidden md:flex justify-between items-center inset-0 mx-auto left-5 right-5">
-              <button className="btn btn-circle " onClick={slidePrev}>
-                ❮
-              </button>
-              <button className="btn btn-circle " onClick={slideNext}>
-                ❯
-              </button>
+            <div className="carousel w-full relative flex flex-col items-center gap-10">
+              <div>
+                <h1 className="text-4xl md:text-5xl font-bold ">Snapshots</h1>
+              </div>
+              <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                  delay: 3000,
+                  disableOnInteraction: false,
+                }}
+                cssMode={true}
+                mousewheel={true}
+                keyboard={true}
+                modules={[Autoplay, Mousewheel, Keyboard]}
+                className="mySwiper w-11/12 md:w-2/3 mx-auto rounded-lg shadow-2xl mb-20"
+                ref={swiperRef}
+              >
+                {slideImagesDark.map((img, index) => (
+                  <SwiperSlide key={index}>
+                    {theme === "wireframe" ? (
+                      <img
+                        src={slideImagesWireframe[index]}
+                        alt={`Slide ${index + 1}`}
+                      />
+                    ) : (
+                      <img src={img} alt={`Slide ${index + 1}`} />
+                    )}
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+
+              <div className="absolute hidden md:flex justify-between items-center inset-0 mx-auto left-5 right-5">
+                <button className="btn btn-circle " onClick={slidePrev}>
+                  ❮
+                </button>
+                <button className="btn btn-circle " onClick={slideNext}>
+                  ❯
+                </button>
+              </div>
             </div>
           </div>
         </div>
-
-        <div
-          className={`relative h-screen flex items-center justify-center ${
-            theme === "wireframe" ? "bg-white" : " bg-black"
-          }`}
-        >
+        <div className={`${theme === "wireframe" ? "" : "bg-gray-950"}`}>
           <div
-            className="absolute inset-0 bg-cover bg-center"
-            style={{
-              backgroundImage: theme === "wireframe" ? "" : `url(${center})`,
-              opacity: 0.7,
-            }}
-          ></div>
-          <div className="hero min-h-screen my-96">
-            <div className="hero-content flex flex-col gap-10 text-center">
-              <h1 className=" text-3xl lg:text-4xl font-bold">FAQs</h1>
-              <div className=" w-full flex flex-col gap-2">
-                <div className="collapse  collapse-arrow bg-base-200">
-                  <input type="radio" name="my-accordion-2" />
-                  <div className="collapse-title  text-base md:text-xl font-medium">
-                    Major Features of CodeVerse
+            className={`relative h-screen flex items-center justify-center mb-10 ${
+              theme === "wireframe" ? "bg-white" : " bg-black"
+            }`}
+          >
+            <div
+              className="absolute inset-0 bg-cover bg-center"
+              style={{
+                backgroundImage: theme === "wireframe" ? "" : `url(${center})`,
+                opacity: 0.7,
+              }}
+            ></div>
+            <div className="hero min-h-screen ">
+              <div className="hero-content flex flex-col gap-10 text-center">
+                <h1 className=" text-3xl lg:text-4xl font-bold">FAQs</h1>
+                <div className=" w-full flex flex-col gap-2">
+                  <div className="collapse  collapse-arrow bg-base-200">
+                    <input type="radio" name="my-accordion-2" />
+                    <div className="collapse-title  text-base md:text-xl font-medium">
+                      Major Features of CodeVerse
+                    </div>
+                    <div className="collapse-content">
+                      <p>
+                        Integration With AI , LocalStorage , User-Friendly
+                        Editors.
+                      </p>
+                    </div>
                   </div>
-                  <div className="collapse-content">
-                    <p>
-                      Integration With AI , LocalStorage , User-Friendly
-                      Editors.
-                    </p>
+                  <div className="collapse collapse-arrow bg-base-200">
+                    <input type="radio" name="my-accordion-2" />
+                    <div className="collapse-title text-base md:text-xl font-medium">
+                      What are the Buttons - HTML CSS JavaScript?
+                    </div>
+                    <div className="collapse-content">
+                      <p>
+                        These buttons are used to adjust their respective
+                        editors.
+                      </p>
+                    </div>
                   </div>
-                </div>
-                <div className="collapse collapse-arrow bg-base-200">
-                  <input type="radio" name="my-accordion-2" />
-                  <div className="collapse-title text-base md:text-xl font-medium">
-                    What are the Buttons - HTML CSS JavaScript?
+                  <div className="collapse collapse-arrow bg-base-200">
+                    <input type="radio" name="my-accordion-2" />
+                    <div className="collapse-title text-base md:text-xl font-medium">
+                      Unique Feature of CodeVerse
+                    </div>
+                    <div className="collapse-content ">
+                      <p className="px-2">
+                        <span className="font-bold">LocalStorage</span> is used
+                        to ensure your code remains intact even after a browser
+                        refresh.
+                      </p>
+                    </div>
                   </div>
-                  <div className="collapse-content">
-                    <p>
-                      These buttons are used to adjust their respective editors.
-                    </p>
+                  <div className="collapse  collapse-arrow bg-base-200">
+                    <input type="radio" name="my-accordion-2" />
+                    <div className="collapse-title text-base md:text-xl font-medium">
+                      Information Regarding AI
+                    </div>
+                    <div className="collapse-content">
+                      <p>
+                        Responses generated from AI are directly parsed into
+                        respective editors.
+                      </p>
+                    </div>
                   </div>
-                </div>
-                <div className="collapse collapse-arrow bg-base-200">
-                  <input type="radio" name="my-accordion-2" />
-                  <div className="collapse-title text-base md:text-xl font-medium">
-                    Unique Feature of CodeVerse
+                  <div className="collapse  collapse-arrow bg-base-200">
+                    <input type="radio" name="my-accordion-2" />
+                    <div className="collapse-title text-base md:text-xl font-medium">
+                      Usage Suggestions Regarding AI
+                    </div>
+                    <div className="collapse-content">
+                      <p>
+                        Type "Write a language program" followed by the language
+                        you prefer.
+                      </p>
+                    </div>
                   </div>
-                  <div className="collapse-content ">
-                    <p className="px-2">
-                      <span className="font-bold">LocalStorage</span> is used to
-                      ensure your code remains intact even after a browser
-                      refresh.
-                    </p>
-                  </div>
-                </div>
-                <div className="collapse  collapse-arrow bg-base-200">
-                  <input type="radio" name="my-accordion-2" />
-                  <div className="collapse-title text-base md:text-xl font-medium">
-                    Information Regarding AI
-                  </div>
-                  <div className="collapse-content">
-                    <p>
-                      Responses generated from AI are directly parsed into
-                      respective editors.
-                    </p>
-                  </div>
-                </div>
-                <div className="collapse  collapse-arrow bg-base-200">
-                  <input type="radio" name="my-accordion-2" />
-                  <div className="collapse-title text-base md:text-xl font-medium">
-                    Usage Suggestions Regarding AI
-                  </div>
-                  <div className="collapse-content">
-                    <p>
-                      Type "Write a language program" followed by the language
-                      you prefer.
-                    </p>
-                  </div>
-                </div>
-                <div className="collapse  collapse-arrow bg-base-200">
-                  <input type="radio" name="my-accordion-2" />
-                  <div className="collapse-title text-base md:text-xl font-medium">
-                    Upcoming Features
-                  </div>
-                  <div className="collapse-content">
-                    <p>
-                      User login for personalized access , Storing codes in
-                      cloud and more Coming soon...
-                    </p>
+                  <div className="collapse  collapse-arrow bg-base-200">
+                    <input type="radio" name="my-accordion-2" />
+                    <div className="collapse-title text-base md:text-xl font-medium">
+                      Upcoming Features
+                    </div>
+                    <div className="collapse-content">
+                      <p>
+                        User login for personalized access , Storing codes in
+                        cloud and more Coming soon...
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div
-          className={`relative h-screen flex items-center justify-center ${
-            theme === "wireframe" ? "bg-white" : " bg-black"
-          }`}
-        >
+        <div className={`${theme === "wireframe" ? "" : "bg-black"}`}>
           <div
-            className="absolute inset-0 bg-cover bg-center"
-            style={{
-              backgroundImage: theme === "wireframe" ? "" : `url(${center})`,
-              opacity: 0.7,
-            }}
-          ></div>
-          <div className="hero min-h-screen my-96">
-            <div className="hero-content flex flex-col gap-20 text-center">
-              <ContactForm theme={theme} />
+            className={`relative h-screen flex items-center justify-center mb-4${
+              theme === "wireframe" ? "bg-white" : " bg-black"
+            }`}
+          >
+            <div
+              className="absolute inset-0 bg-cover bg-center"
+              style={{
+                backgroundImage: theme === "wireframe" ? "" : `url(${center})`,
+                opacity: 0.7,
+              }}
+            ></div>
+            <div className="hero min-h-screen ">
+              <div className="hero-content flex flex-col gap-20 text-center">
+                <ContactForm theme={theme} />
+              </div>
             </div>
           </div>
         </div>
